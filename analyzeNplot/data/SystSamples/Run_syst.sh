a59e858a7d7a9305f149885c47d9003d67a54e40
Andris Potrebko <andris.potrebko@gmail.com>
2021-01-14 17:07:44 -0600

    Added systemmatics for alternate samples

---
+++ analyzeNplot/data/SystSamples/Run_syst.sh
@@ -0,0 +1,6 @@
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_FSR_down.json -o systematics/syst_FSR_down -n 2
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_FSR_up.json -o systematics/syst_FSR_up -n 2
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_ISR_down.json -o systematics/syst_ISR_down -n 2
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_ISR_up.json -o systematics/syst_ISR_up -n 2
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_scale_down.json -o systematics/syst_scale_down -n 2
+python runBJetEnergyPeak.py -i /store/user/cmsdas/2021/long_exercises/TopMass -j data/SystSamples/sample_scale_up.json -o systematics/syst_scale_up -n 2
